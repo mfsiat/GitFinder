5ee31d46a4b36b492c557691416b9b9d09de3536
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-11-03 01:46:51 +0600

    updated and added alert

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Init GitHub 
 const github = new GitHub;
 
+// init UI 
+const ui = new UI;
+
 // Search input
 const searchUser = document.getElementById("searchUser");
 
@@ -14,11 +17,14 @@ searchUser.addEventListener("keyup", e => {
     github.getUser(userText).then(data => {
       if(data.profile.message === 'Not Found'){
         // Show alert 
+        ui.showAlert('User not found', 'alert alert-danger');
       } else {
         // Show profile 
+        ui.showProfile(data.profile);
       }
     })
   } else {
     // Clear profile 
+    ui.clearProfile();
   }
 });
