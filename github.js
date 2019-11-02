c5637f1c13b8d0d3c4303cf65245ed10af4e35a0
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-11-03 02:15:53 +0600

    added repos and completed

--- github.js
+++ github.js
@@ -3,6 +3,8 @@ class GitHub {
   constructor() {
     this.client_id = "98018f9a0b3ef926a8e1";
     this.client_secret = "e9f21488fbfc7732c0920267050fd1ae3de74802";
+    this.repos_count = 10;
+    this.repos_sort = 'created: asc';
   }
 
   // get user method
@@ -13,10 +15,14 @@ class GitHub {
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile
+      profile,
+      repos
     }
   }
 }
--- ui.js
+++ ui.js
@@ -4,7 +4,7 @@ class UI {
     this.profile = document.getElementById("profile");
   }
 
-  // Display Profile 
+  // Display Profile
   showProfile(user) {
     this.profile.innerHTML = `
       <div class="card card-body mb-3">
@@ -33,40 +33,66 @@ class UI {
     `;
   }
 
-  // Show alert message 
+  // Show alert message
   showAlert(message, className) {
-    // Clear any remaining alert 
+    // Clear any remaining alert
     this.cleartAlert();
 
-    // Create Div 
-    const div = document.createElement('div');
-    // Add classes 
+    // Create Div
+    const div = document.createElement("div");
+    // Add classes
     div.className = className;
-    // Add Text 
+    // Add Text
     div.appendChild(document.createTextNode(message));
-    // Get parent 
-    const container = document.querySelector('.searchContainer');
-    // Get search box 
-    const search = document.querySelector('.search');
-    // Insert alert 
+    // Get parent
+    const container = document.querySelector(".searchContainer");
+    // Get search box
+    const search = document.querySelector(".search");
+    // Insert alert
     container.insertBefore(div, search);
 
-    // Timeout after 3 sec 
+    // Timeout after 3 sec
     setTimeout(() => {
       this.cleartAlert();
     }, 3000);
   }
 
-  // Clear alert message 
-  cleartAlert() {
-    const currentAlert = document.querySelector('.alert');
+  // Show user repos
+  showRepos(repos) {
+    let output = "";
 
-    if(currentAlert){
+    repos.forEach(function(repo) {
+      output += `
+        <div class="card card-body mb-2">
+          <div class="row">
+            <div class="col-md-6">
+              <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+            </div>  
+            <div class="col-md-6">
+              <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+              <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
+              <span class="badge badge-success">Forks: ${repo.forks_count}</span>              
+              <span class="badge badge-success">Language: ${repo.language}</span>              
+            </div>  
+          </div>
+        </div>
+      `;
+    });
+
+    // Output repos
+    document.getElementById("repos").innerHTML = output;
+  }
+
+  // Clear alert message
+  cleartAlert() {
+    const currentAlert = document.querySelector(".alert");
+
+    if (currentAlert) {
       currentAlert.remove();
     }
   }
 
-  // Clear Profile 
+  // Clear Profile
   clearProfile() {
     this.profile.innerHTML = "";
   }
